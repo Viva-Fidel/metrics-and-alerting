cd7d710b843c7a6eb8ea337862883a419f2f4e83
alexis <alexis.artamonov@gmail.com>
2026-03-14 17:40:31 +0300

    fix: правка запуска сервера

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -14,7 +14,7 @@ func parseFlags(conf *config.Config) {
 
     flag.Parse()
 
-    if envRunAddr := conf.Server.Address; envRunAddr != "" {
-        flagRunAddr = envRunAddr
+    if conf.Server.Address != "" {
+        flagRunAddr = conf.Server.Address
     }
 }
