d6429723fc222db2b25930ad3ef32a674f963955
alexis <alexis.artamonov@gmail.com>
2026-03-14 17:31:59 +0300

    fix: правка запуска агента

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,7 +12,7 @@ type Config struct {
 }
 
 type AgentConfig struct {
-	Address     string `env:"ADDRESS" envDefault:"localhost:8081"`
+	Address     string `env:"ADDRESS" envDefault:"localhost:8080"`
 	ReportInterval int64 `env:"REPORT_INTERVAL" envDefault:"10"`
 	PollInterval   int64 `env:"POLL_INTERVAL" envDefault:"2"`
 }
