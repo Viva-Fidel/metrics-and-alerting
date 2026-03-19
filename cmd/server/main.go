800affd5e6d1a7f7daca783196e74a90debc62d5
alexis <alexis.artamonov@gmail.com>
2026-03-19 14:47:38 +0300

    feat: поменян логгер на slog

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"log/slog"
+	"os"
 
 	"github.com/Viva-Fidel/metrics-and-alerting/internal/config"
 	"github.com/Viva-Fidel/metrics-and-alerting/internal/handler"
+	"github.com/Viva-Fidel/metrics-and-alerting/internal/logger"
 	"github.com/Viva-Fidel/metrics-and-alerting/internal/repository"
 	"github.com/gin-gonic/gin"
 )
@@ -13,19 +14,14 @@ import (
 
 func setupRouter() *gin.Engine {
 
-	router := gin.New()
-	router.Use(gin.Recovery())
+	newLogger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	// кастомный логгер gin
-	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-    return fmt.Sprintf("[%s] %s %s -> %d %d bytes in %v\n",
-        param.TimeStamp.Format(time.RFC3339),
-        param.Method,
-        param.Path,
-        param.StatusCode,
-        param.BodySize,
-        param.Latency,
-    )}))
+
+	router := gin.New()
+
+	// logger
+    router.Use(logger.SlogMiddleware(newLogger))
+    router.Use(gin.Recovery())
 	
 
     // storages
